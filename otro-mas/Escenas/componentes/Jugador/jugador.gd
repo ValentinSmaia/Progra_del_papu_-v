1cb41d0f9cc309b75aac7685b6fb99c9de969f72
ValentinSmaia <valentin.smaiaechevarria@gmail.com>
2025-03-20 12:40:52 -0300

    Maquina de estado

--- otro-mas/Escenas/componentes/Jugador/jugador.gd
+++ otro-mas/Escenas/componentes/Jugador/jugador.gd
@@ -3,6 +3,6 @@ extends CharacterBody2D
 @export var velocidad = 1000
 
 func _physics_process(delta):
-	var direccion = Input.get_axis ("derecha", "izquierda")
+	var direccion = Input.get_axis ("izquierda", "derecha")
 	velocity.x = direccion * velocidad
 	move_and_slide()
